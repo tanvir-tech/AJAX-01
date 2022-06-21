9d9bee956c1c4480d2e0672d63e14dc0f768848d
tanvir-tech <tanvirictbd@gmail.com>
2022-06-21 17:36:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+function sendRequest(method, url) {
+
+    const promise = new Promise((resolve, reject) => {
+        
+        const xhr = new XMLHttpRequest();
+
+        xhr.onload = function () {
+            // console.log(this.response);
+            resolve(this.response);
+        };
+
+        xhr.open(method, url);
+
+        xhr.responseType = "json";
+
+        xhr.send();
+    });
+
+
+    return promise;
+}
+
+
+function getData() {
+    // alert("get data");
+    // const xhr = new XMLHttpRequest();
+
+    // xhr.onload = function(){
+    //     console.log(this.responseText);
+    // };
+
+    // xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
+
+    // xhr.send();
+
+    sendRequest("GET","https://jsonplaceholder.typicode.com/todos/1").then((responseData)=>{
+        console.log(responseData);
+    });
+
+}
+
+function setData() {
+    alert("set data");
+}
+
+
+$("#get").click(function (e) {
+    e.preventDefault();
+    getData();
+});
+
+
+$("#set").click(function (e) {
+    e.preventDefault();
+    setData();
+});
